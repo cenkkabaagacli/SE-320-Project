e6cbc6410e249a76b2b3daa03de6f7a4c9347ce6
Hmbayrakdar <hamzamelih61@hotmail.com>
2017-11-30 13:18:31 +0300

    Revert "Level Design"
    This reverts commit b56de90a00f3d96c06cd28f18f6cecf873e47f79.

--- Assets/Scripts/MovingTest.cs
+++ Assets/Scripts/MovingTest.cs
@@ -3,40 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MovingTest : MonoBehaviour
-{
-    NavMeshAgent agent;
-    public GameObject goTerrain;
-    // Use this for initialization
-    void Start()
-    {
+public class MovingTest : MonoBehaviour {
+	NavMeshAgent agent;
+	public GameObject goTerrain;
+	// Use this for initialization
+	void Start () {
+		
+	}
 
-    }
+	void Awake(){
+		agent = GetComponent<NavMeshAgent>();
+	}
 
-    void Awake()
-    {
-        agent = GetComponent<NavMeshAgent>();
-    }
+	// Update is called once per frame
+	void Update () {
+		
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-    // Update is called once per frame
-    void Update()
-    {
+		if (Input.GetMouseButton (1)) {
+			if (goTerrain.GetComponent<Collider> ().Raycast (ray, out hit, Mathf.Infinity)) {
+				agent.destination = hit.point;
+				agent.isStopped = false;
+			}
+		}
 
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Input.GetMouseButton(1))
-        {
-            if (goTerrain.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
-            {
-                agent.destination = hit.point;
-                agent.isStopped = false;
-            }
-        }
-
-    }
+	}	
 }
-
-
-
-
