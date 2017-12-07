1ae171027680ca525c23ba24fec328c0f4b72ea0
Cenk Kabaağaçlı <cenk96.lemur@hotmail.com>
2017-12-07 14:06:31 +0300

    Butona tıklayıp oyuna başlama
    "Start New Game" butonuna basınca fristLevel yükleniyo

---
+++ Assets/Scripts/ChangeSceneToLevel1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneToLevel1 : MonoBehaviour
+{
+
+    public void ChangeSceenToLevel1(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
