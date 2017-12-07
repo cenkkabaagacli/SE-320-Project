1a25a009d8a4957221ae6179fd7d59ab7c0db2f4
Cenk Kabaağaçlı <cenk96.lemur@hotmail.com>
2017-12-07 15:09:31 +0300

    Health Bar ve Second Level
    Health, scriptle değisiyor.
    Second Level oluşturuldu.

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Slider mySlider;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mySlider.value = 0.15f;
+        Debug.Log(mySlider.value);
+	}
+}
