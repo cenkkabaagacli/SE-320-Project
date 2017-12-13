c293460e878781f1d87e306c4caf42262c599d06
Cenk Kabaağaçlı <cenk96.lemur@hotmail.com>
2017-12-13 21:55:39 +0300

    GUI Panels
    -Click 'C' to show/hide character attributes
    -Click 'Esc' to show/hide pause menu

---
+++ Assets/Scripts/GUIPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIPanel : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public GameObject CharAtt;
+    public GameObject Pause;
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            CharAtt.SetActive(!CharAtt.activeInHierarchy);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause.SetActive(!Pause.activeInHierarchy);
+        }
+    }
+}
