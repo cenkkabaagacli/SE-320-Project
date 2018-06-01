72554b5617e42189f1e7d99cf2a5f412051ebd73
hmbayrakdar <hamzamelih61@hotmail.com>
2018-06-01 12:11:00 +0300

    Cooldown for the skill and teleportation to boss room and starting area
    6 Seconds cooldown for the skill in PlayerMove script.
    Functions for teleporting Character Script.

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -1,28 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class CharacterScript : MonoBehaviour
 {
 
     public int attack = 20;
-	public int skillAttack = 40;
+    public int skillAttack = 40;
     public int defence = 5;
     public float health = 100f;
     private int exp = 0;
     public int level = 1;
     private int levelCounter = 0;
     private int expNeed = 100;
-	public bool isDead = false;
+    public bool isDead = false;
     public int healthPotAmount = 3;
     public GameObject GameOver;
     public GameObject endGameSound;
     public GameObject mapSound;
-	private int layerMask = 8;
-	public GameObject hitEffect;
-	public AudioClip[] hitAudioSources;
-	private bool isNPCHit = false;
+    private int layerMask = 8;
+    public GameObject hitEffect;
+    public AudioClip[] hitAudioSources;
+    private bool isNPCHit = false;
 
     // Use this for initialization
     void Start()
@@ -30,22 +31,22 @@ public class CharacterScript : MonoBehaviour
 		
     }
 
-	IEnumerator WaitForEndScreen(){
-		yield return new WaitForSecondsRealtime (1.19f);
-		GameOver.SetActive(true);
-		endGameSound.SetActive(true);
-		mapSound.SetActive(false);
-	}
+    IEnumerator WaitForEndScreen(){
+        yield return new WaitForSecondsRealtime (1.19f);
+        GameOver.SetActive(true);
+        endGameSound.SetActive(true);
+        mapSound.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
-		if(isDead==false){
-			DeathCheck();
-		}
+        if(isDead==false){
+            DeathCheck();
+        }
 
         //deneme için yazdım, silincek
-		if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L))
         {
             SetExp(20);
             Debug.Log("exp + 20");
@@ -54,19 +55,20 @@ public class CharacterScript : MonoBehaviour
         //can potu kullanma
         if (Input.GetKeyDown(KeyCode.H))
         {	
-			if (healthPotAmount > 0) {
-				UseHealthPot ();
-				Debug.Log ("Used health pot");
-			}
+            if (healthPotAmount > 0) {
+                UseHealthPot ();
+                Debug.Log ("Used health pot");
+            }
         }
 
-	    if (Input.GetKeyDown(KeyCode.R))
-	    {
-		    if (isNPCHit == true)
-		    {
-			    Debug.Log("domat");
-		    }
-	    }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (isNPCHit == true)
+            {
+                Debug.Log("domat");
+            }
+        }
+
     }
 
     public void SetExp(int myExp)
@@ -74,16 +76,16 @@ public class CharacterScript : MonoBehaviour
         exp = exp + myExp;
         if (exp >= expNeed)
         {
-			int surplusExp;
+            int surplusExp;
             level++;
             levelCounter++;
             Debug.Log("level up");
             GameObject.Find("Pause").GetComponent<GUIPanel>().AttributeButtons(levelCounter);
-			surplusExp = exp - 100;
-			exp = 0 + surplusExp;
-			health = health + 30;
-			if (health > 100)
-				health = 100;
+            surplusExp = exp - 100;
+            exp = 0 + surplusExp;
+            health = health + 30;
+            if (health > 100)
+                health = 100;
             healthPotAmount++;
         }
 
@@ -119,73 +121,73 @@ public class CharacterScript : MonoBehaviour
         GameObject.Find("Pause").GetComponent<GUIPanel>().AttributeButtons(levelCounter);
     }
 
-	public void DeathCheck(){
-		if (health <= 0) {
-			GetComponent<PlayerMove> ().enabled = false;
-			GetComponent<Animation> ().Play ("death");
-			StartCoroutine (WaitForEndScreen ());
-			isDead = true;
+    public void DeathCheck(){
+        if (health <= 0) {
+            GetComponent<PlayerMove> ().enabled = false;
+            GetComponent<Animation> ().Play ("death");
+            StartCoroutine (WaitForEndScreen ());
+            isDead = true;
             
         }
-	}
+    }
 
-	public void SkillAttack()
-	{
-		Vector3 center = GameObject.Find("SkillSphereCastAllSource").transform.position;
-		float radius = 4f;
+    public void SkillAttack()
+    {
+        Vector3 center = GameObject.Find("SkillSphereCastAllSource").transform.position;
+        float radius = 4f;
 		
-		/*RaycastHit[] Physics.SphereCastAll(source, radius, new Vector3(0,0,0), 1f, int layerMask = 8, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore); 
+        /*RaycastHit[] Physics.SphereCastAll(source, radius, new Vector3(0,0,0), 1f, int layerMask = 8, QueryTriggerInteraction queryTriggerInteraction = QueryTriggerInteraction.Ignore); 
+        
+        RaycastHit2D[] CircleCastAll(source, radius, new Vector2(1,1), 3f, layerMask );*/
 		
-		RaycastHit2D[] CircleCastAll(source, radius, new Vector2(1,1), 3f, layerMask );*/
-		
-		Collider[] hitColliders = Physics.OverlapSphere(center, radius);
-		int i = 0;
-		while (i < hitColliders.Length)
-		{
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
 			
-			if (hitColliders[i].transform.tag == "Enemy") {
+            if (hitColliders[i].transform.tag == "Enemy") {
 
 			
-				if (hitColliders[i].transform.gameObject.GetComponent<WolfScript>() != null) {
-				hitColliders[i].transform.gameObject.GetComponent<WolfScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
-				Vector3 hitpos = hitColliders[i].transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, 5)];
-				GetComponent<AudioSource> ().Play ();
-				}
+                if (hitColliders[i].transform.gameObject.GetComponent<WolfScript>() != null) {
+                    hitColliders[i].transform.gameObject.GetComponent<WolfScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
+                    Vector3 hitpos = hitColliders[i].transform.position;
+                    hitpos += Vector3.up;
+                    Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
+                    GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, 5)];
+                    GetComponent<AudioSource> ().Play ();
+                }
 			
-				if (hitColliders[i].transform.gameObject.GetComponent<GoblinScript>() != null) {
-				hitColliders[i].transform.gameObject.GetComponent<GoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
-				Vector3 hitpos = hitColliders[i].transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-				}
+                if (hitColliders[i].transform.gameObject.GetComponent<GoblinScript>() != null) {
+                    hitColliders[i].transform.gameObject.GetComponent<GoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
+                    Vector3 hitpos = hitColliders[i].transform.position;
+                    hitpos += Vector3.up;
+                    Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
+                    GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                    GetComponent<AudioSource> ().Play ();
+                }
 			
-				if (hitColliders[i].transform.gameObject.GetComponent<HobgoblinScript>() != null) {
-				hitColliders[i].transform.gameObject.GetComponent<HobgoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
-				Vector3 hitpos = hitColliders[i].transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-				}
+                if (hitColliders[i].transform.gameObject.GetComponent<HobgoblinScript>() != null) {
+                    hitColliders[i].transform.gameObject.GetComponent<HobgoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
+                    Vector3 hitpos = hitColliders[i].transform.position;
+                    hitpos += Vector3.up;
+                    Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
+                    GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                    GetComponent<AudioSource> ().Play ();
+                }
 			
-				if (hitColliders[i].transform.gameObject.GetComponent<TrollScript>() != null) {
-				hitColliders[i].transform.gameObject.GetComponent<TrollScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
-				Vector3 hitpos = hitColliders[i].transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-				}
-			}
+                if (hitColliders[i].transform.gameObject.GetComponent<TrollScript>() != null) {
+                    hitColliders[i].transform.gameObject.GetComponent<TrollScript> ().GetAttacked (GetComponent<CharacterScript> ().skillAttack);
+                    Vector3 hitpos = hitColliders[i].transform.position;
+                    hitpos += Vector3.up;
+                    Instantiate (hitEffect, hitpos, hitColliders[i].transform.rotation);
+                    GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                    GetComponent<AudioSource> ().Play ();
+                }
+            }
 			
-			i++;
-		}
-	}
+            i++;
+        }
+    }
 
     public void UseHealthPot()
     {
@@ -193,23 +195,37 @@ public class CharacterScript : MonoBehaviour
         health = 100;
     }
 	
-	private void OnTriggerEnter(Collider npc)
-     {
-     	if (npc.gameObject.name == "Barbarian shaman")
-	     {
-		     Debug.Log("npc - true");
+    private void OnTriggerEnter(Collider npc)
+    {
+        if (npc.gameObject.name == "Barbarian shaman")
+        {
+            Debug.Log("npc - true");
 
-		     isNPCHit = true;
-	     }
-     }
+            isNPCHit = true;
+        }
+    }
 
-	private void OnTriggerExit(Collider npc)
-	{
-		if (npc.gameObject.name == "Barbarian shaman")
-		{
-			Debug.Log("npc - false");
+    private void OnTriggerExit(Collider npc)
+    {
+        if (npc.gameObject.name == "Barbarian shaman")
+        {
+            Debug.Log("npc - false");
 
-			isNPCHit = false;
-		}
-	}
+            isNPCHit = false;
+        }
+    }
+
+    public void GoToBossRoom()
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        transform.position= new Vector3(1060,0,110);
+        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = true;
+    }
+
+    public void GoBackToStartingMap()
+    {
+        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        transform.position= new Vector3(19,0,9);
+        GetComponent<UnityEngine.AI.NavMeshAgent> ().enabled = true;
+    }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,18 +7,22 @@ public class PlayerMove : MonoBehaviour
     public int curAnimClip;
     public float maxVerticalSpeed = 7;
     private float maxHorizontalSpeed = 60;
-	private float attackRange;
-	public GameObject hitEffect;
-	public AudioClip[] hitAudioSources;
+    private float attackRange;
+    public GameObject hitEffect;
+    public AudioClip[] hitAudioSources;
+    private float SkillCoolDown = 6;
+    private float SkillActivation = 0;
+    public float SkillTimeReadyAsSeconds;
+	
 
-	//private Vector3 currentLocation;
-	//private Vector3 previousLocation;
-	//public float lookSpeed= 10;
+    //private Vector3 currentLocation;
+    //private Vector3 previousLocation;
+    //public float lookSpeed= 10;
     // Use this for initialization
     void Start()
     {
-		attackRange = 6;
-		gameObject.GetComponent<Animation> ().Play ("free");
+        attackRange = 6;
+        gameObject.GetComponent<Animation> ().Play ("free");
     }
 
 
@@ -34,10 +38,12 @@ public class PlayerMove : MonoBehaviour
 
         float upSpeed = Input.GetAxis("Vertical");
 
+        SkillTimeReadyAsSeconds = SkillActivation - Time.time;
+
         transform.Translate(0, 0, maxVerticalSpeed * upSpeed * Time.deltaTime);
         transform.Rotate(0, maxHorizontalSpeed * rightSpeed * Time.deltaTime, 0);
 
-		if (!gameObject.GetComponent<Animation>().IsPlaying("attack") && !gameObject.GetComponent<Animation>().IsPlaying("skill"))
+        if (!gameObject.GetComponent<Animation>().IsPlaying("attack") && !gameObject.GetComponent<Animation>().IsPlaying("skill"))
         {
             if (rightSpeed != 0.0f || upSpeed != 0.0f)
             {
@@ -49,147 +55,148 @@ public class PlayerMove : MonoBehaviour
             }
         }
 	
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			gameObject.GetComponent<Animation> ().Play ("attack");
-		}
-
-	    if (Input.GetKeyDown(KeyCode.E))
-	    {
-		    gameObject.GetComponent<Animation> ().Play ("skill");
-	    }
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            gameObject.GetComponent<Animation> ().Play ("attack");
+        }
+		
+        if (Input.GetKeyDown(KeyCode.E) && SkillTimeReadyAsSeconds < 0)
+        {
+            gameObject.GetComponent<Animation> ().Play ("skill");
+            SkillActivation = Time.time + SkillCoolDown;
+        }
     }
 
     void OnTriggerStay(Collider other)
-	{
-		if (other.GetComponent<UnityEngine.AI.NavMeshAgent> () != null) {
-			if (other.GetComponent<UnityEngine.AI.NavMeshAgent> ().isActiveAndEnabled) {
-				other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent> ().destination = transform.position;
+    {
+        if (other.GetComponent<UnityEngine.AI.NavMeshAgent> () != null) {
+            if (other.GetComponent<UnityEngine.AI.NavMeshAgent> ().isActiveAndEnabled) {
+                other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent> ().destination = transform.position;
 
-				if (other.gameObject.GetComponent<WolfScript> () != null) {
-					other.gameObject.GetComponent<WolfScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<WolfScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<WolfScript> ().isdead == false) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}
-					}
-				}
+                if (other.gameObject.GetComponent<WolfScript> () != null) {
+                    other.gameObject.GetComponent<WolfScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<WolfScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<WolfScript> ().isdead == false) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }
+                    }
+                }
 
-				if (other.gameObject.GetComponent<GoblinScript> () != null) {
-					other.gameObject.GetComponent<GoblinScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<GoblinScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<GoblinScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}            
-					}
-				}
+                if (other.gameObject.GetComponent<GoblinScript> () != null) {
+                    other.gameObject.GetComponent<GoblinScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<GoblinScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<GoblinScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }            
+                    }
+                }
 
-				if (other.gameObject.GetComponent<HobgoblinScript> () != null) {
-					other.gameObject.GetComponent<HobgoblinScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<HobgoblinScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<HobgoblinScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}  
-					}
-				}
+                if (other.gameObject.GetComponent<HobgoblinScript> () != null) {
+                    other.gameObject.GetComponent<HobgoblinScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<HobgoblinScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<HobgoblinScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }  
+                    }
+                }
 
-				if (other.gameObject.GetComponent<TrollScript> () != null) {
-					other.gameObject.GetComponent<TrollScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<TrollScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<TrollScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}  
-					}
-				}
-			}
-		}
-	}
+                if (other.gameObject.GetComponent<TrollScript> () != null) {
+                    other.gameObject.GetComponent<TrollScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<TrollScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<TrollScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }  
+                    }
+                }
+            }
+        }
+    }
 
-	void OnTriggerEnter(Collider other)
-	{
-		if (other.GetComponent<UnityEngine.AI.NavMeshAgent> () != null) {
-			if (other.GetComponent<UnityEngine.AI.NavMeshAgent> ().isActiveAndEnabled) {
-				other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent> ().destination = transform.position;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<UnityEngine.AI.NavMeshAgent> () != null) {
+            if (other.GetComponent<UnityEngine.AI.NavMeshAgent> ().isActiveAndEnabled) {
+                other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent> ().destination = transform.position;
 
-				if (other.gameObject.GetComponent<WolfScript> () != null) {
-					other.gameObject.GetComponent<WolfScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<WolfScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<WolfScript> ().isdead == false) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}
-					}
-				}
+                if (other.gameObject.GetComponent<WolfScript> () != null) {
+                    other.gameObject.GetComponent<WolfScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<WolfScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<WolfScript> ().isdead == false) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }
+                    }
+                }
 
-				if (other.gameObject.GetComponent<GoblinScript> () != null) {
-					other.gameObject.GetComponent<GoblinScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<GoblinScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<GoblinScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}            
-					}
-				}
+                if (other.gameObject.GetComponent<GoblinScript> () != null) {
+                    other.gameObject.GetComponent<GoblinScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<GoblinScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<GoblinScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }            
+                    }
+                }
 
-				if (other.gameObject.GetComponent<HobgoblinScript> () != null) {
-					other.gameObject.GetComponent<HobgoblinScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<HobgoblinScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<HobgoblinScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}  
-					}
-				}
+                if (other.gameObject.GetComponent<HobgoblinScript> () != null) {
+                    other.gameObject.GetComponent<HobgoblinScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<HobgoblinScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<HobgoblinScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }  
+                    }
+                }
 
-				if (other.gameObject.GetComponent<TrollScript> () != null) {
-					other.gameObject.GetComponent<TrollScript> ().setisMoving (true);
-					if (other.gameObject.GetComponent<TrollScript> ().getisattacking ()) {
-						if (other.gameObject.GetComponent<TrollScript> ().isdead) {
-							other.gameObject.transform.LookAt (gameObject.transform);
-						}  
-					}
-				}
-			}
-		}
-	}
+                if (other.gameObject.GetComponent<TrollScript> () != null) {
+                    other.gameObject.GetComponent<TrollScript> ().setisMoving (true);
+                    if (other.gameObject.GetComponent<TrollScript> ().getisattacking ()) {
+                        if (other.gameObject.GetComponent<TrollScript> ().isdead) {
+                            other.gameObject.transform.LookAt (gameObject.transform);
+                        }  
+                    }
+                }
+            }
+        }
+    }
 
-	void CheckEnemyInFront(){
-		RaycastHit hit;
+    void CheckEnemyInFront(){
+        RaycastHit hit;
 
-		Vector3 fwd = GameObject.Find("SphereCastSource").transform.TransformDirection (Vector3.forward);
-		float thickness = 2f;
-		if (Physics.SphereCast (GameObject.Find("SphereCastSource").transform.position,thickness, fwd, out hit, attackRange) && hit.transform.tag == "Enemy") {
+        Vector3 fwd = GameObject.Find("SphereCastSource").transform.TransformDirection (Vector3.forward);
+        float thickness = 2f;
+        if (Physics.SphereCast (GameObject.Find("SphereCastSource").transform.position,thickness, fwd, out hit, attackRange) && hit.transform.tag == "Enemy") {
 
-			if (hit.transform.gameObject.GetComponent<WolfScript>() != null) {
-				hit.transform.gameObject.GetComponent<WolfScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
-				Vector3 hitpos = hit.transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hit.transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-			}
-			if (hit.transform.gameObject.GetComponent<GoblinScript>() != null) {
-				hit.transform.gameObject.GetComponent<GoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
-				Vector3 hitpos = hit.transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hit.transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-			}
-			if (hit.transform.gameObject.GetComponent<HobgoblinScript>() != null) {
-				hit.transform.gameObject.GetComponent<HobgoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
-				Vector3 hitpos = hit.transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hit.transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-			}
-			if (hit.transform.gameObject.GetComponent<TrollScript>() != null) {
-				hit.transform.gameObject.GetComponent<TrollScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
-				Vector3 hitpos = hit.transform.position;
-				hitpos += Vector3.up;
-				Instantiate (hitEffect, hitpos, hit.transform.rotation);
-				GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
-				GetComponent<AudioSource> ().Play ();
-			}
-		}
-	}
+            if (hit.transform.gameObject.GetComponent<WolfScript>() != null) {
+                hit.transform.gameObject.GetComponent<WolfScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
+                Vector3 hitpos = hit.transform.position;
+                hitpos += Vector3.up;
+                Instantiate (hitEffect, hitpos, hit.transform.rotation);
+                GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                GetComponent<AudioSource> ().Play ();
+            }
+            if (hit.transform.gameObject.GetComponent<GoblinScript>() != null) {
+                hit.transform.gameObject.GetComponent<GoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
+                Vector3 hitpos = hit.transform.position;
+                hitpos += Vector3.up;
+                Instantiate (hitEffect, hitpos, hit.transform.rotation);
+                GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                GetComponent<AudioSource> ().Play ();
+            }
+            if (hit.transform.gameObject.GetComponent<HobgoblinScript>() != null) {
+                hit.transform.gameObject.GetComponent<HobgoblinScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
+                Vector3 hitpos = hit.transform.position;
+                hitpos += Vector3.up;
+                Instantiate (hitEffect, hitpos, hit.transform.rotation);
+                GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                GetComponent<AudioSource> ().Play ();
+            }
+            if (hit.transform.gameObject.GetComponent<TrollScript>() != null) {
+                hit.transform.gameObject.GetComponent<TrollScript> ().GetAttacked (GetComponent<CharacterScript> ().attack);
+                Vector3 hitpos = hit.transform.position;
+                hitpos += Vector3.up;
+                Instantiate (hitEffect, hitpos, hit.transform.rotation);
+                GetComponent<AudioSource> ().clip = hitAudioSources [Random.Range (0, hitAudioSources.Length)];
+                GetComponent<AudioSource> ().Play ();
+            }
+        }
+    }
 		
 
 }
